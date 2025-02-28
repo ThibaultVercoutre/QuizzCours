e15ef6f7546b19062a1c2f5cbfc401dcafb6b576
Thibault Vercoutre <vercoutre.thibault@gmail.com>
2025-02-28 17:28:12 -0500

    maj backend

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -8,8 +8,8 @@ const sequelize = new Sequelize({
     database: process.env.DB_NAME || 'quizz',
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3306,
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: Number(process.env.DB_PORT) || 3307,
     dialect: 'mysql',
     models: [path.join(__dirname, '..', 'models')], // chemin vers les modèles
     logging: false, // désactive les logs SQL en développement
